7b43d2d656ec12de4585298c775bf091f071f718
Praveen Mehta <praveen.mehta010@gmail.com>
2023-01-08 18:24:36 +0530

    home screen with simple UI

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/functions/get_news_data.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,8 +16,52 @@ class _HomeScreenState extends State<HomeScreen> {
     super.initState();
     getNewsData();
   }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    var w = MediaQuery.of(context).size.width;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xff000000),
+        title: Text(
+          "HEADLINES",
+          style: GoogleFonts.roboto(
+              fontSize: 29,
+              fontWeight: FontWeight.bold,
+              color: Color(0xffffffff),
+              letterSpacing: 3.5),
+        ),
+        centerTitle: true,
+      ),
+      body: Padding(
+          padding: const EdgeInsets.only(top: 16),
+          child: FutureBuilder<List>(
+            future: getNewsData(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: snapshot.data!.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      margin: EdgeInsets.only(left: 16, right: 16, bottom: 24),
+                      shadowColor: Colors.black,
+                      elevation: 10,
+                      color: Colors.blue,
+                      child: Container(
+                        width: w,
+                        height: 400,
+                      ),
+                    );
+                  },
+                );
+              } else {
+                print(snapshot.error);
+              }
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            },
+          )),
+    );
   }
 }
