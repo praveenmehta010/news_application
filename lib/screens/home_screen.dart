46ab339b7623b076f5946ea39b193837ab503552
Praveen Mehta <praveen.mehta010@gmail.com>
2023-01-08 18:09:27 +0530

    got the api data

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/functions/get_news_data.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -9,6 +10,11 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getNewsData();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold();
